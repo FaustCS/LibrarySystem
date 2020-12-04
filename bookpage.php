cab1d3ee4ff21be82c54312f488f3d78cfdfc26c
Mxkxk <74319751+Mxkxk@users.noreply.github.com>
2020-12-04 21:54:52 +0200

    Add support of jpg/jpeg cover image

--- bookpage.php
+++ bookpage.php
@@ -1,50 +1,10 @@
-<<<<<<< HEAD
-<?php
-session_start();
-require_once 'components\header.php';
-require_once 'classes\Catalog.php';
-
-$data = $editAndDelete->getInfo($_GET['book_id']);
-?>
-    <script>
-        document.title = <?php echo "'" . $data[0][1] . "'" ?>;
-    </script>
-    <h1 style="text-align:center;font-size:2em;"><?php echo $data[0][1] ?></h1>
-    <div class="container">
-        <div style="
-			width:60%;
-			box-sizing:border-box;
-			float:left;
-		">
-            <img style="
-			padding-right:1em;
-			box-sizing:border-box;
-			float:right;
-			" src="img/cover/<?php echo $data[0][0] ?>.png" width="300" height="420" alt="<?php echo "Обкладинка " . $data[0][1] ?>">
-        </div>
-        <div style="
-			width:40%;
-			box-sizing:border-box;
-			float:right;
-			font-size:1.3em;
-		">
-            <p>Назва: <?php echo $data[0][1] ?></p>
-            <p>Видавництво: <?php echo $data[0][2] ?></p>
-            <p>Жанр: <?php echo $data[0][3] ?></p>
-            <p>Кількість сторінок: <?php echo $data[0][4] ?></p>
-            <p>В наявності: <?php echo $data[0][5] ?></p>
-        </div>
-    </div>
-<?php
-require_once 'components\footer.php';
-?>
-=======
 <?php
 session_start();
 $err=[];
 
 require_once 'components\header.php';
 require_once 'classes\Catalog.php';
+//echo $_SESSION['UserId']."<br>";
 
 $data=$editAndDelete->getInfo($_GET['book_id']);
 
@@ -74,7 +34,13 @@ $dataReview = $review->getReview($_GET['book_id']);
 			padding-right:1em;
 			box-sizing:border-box;
 			float:right;
-			" src = "img/cover/<?php echo $data[0][0]?>.png" height="420" width="300" alt="<?php echo "Обкладинка " . $data[0][1]?>">
+			" src = "img/cover/
+			<?php 
+			echo $data[0][0];
+			if(file_exists("img/cover/" . $data[0][0] . ".png"))echo ".png";
+			if(file_exists("img/cover/" . $data[0][0] . ".jpg"))echo ".jpg";
+			if(file_exists("img/cover/" . $data[0][0] . ".jpeg"))echo ".jpeg";?>
+			" height="420" width="300" alt="<?php echo "Обкладинка " . $data[0][1]?>">
 		</div>		
 		<div style="
 			width:40%;
@@ -87,7 +53,8 @@ $dataReview = $review->getReview($_GET['book_id']);
 			<p>Жанр: <?php echo $data[0][3]?></p>
 			<p>Кількість сторінок: <?php echo $data[0][4] ?></p>
 			<p>В наявності: <?php echo $data[0][5] ?></p>
-		</div>		
+		</div>
+		<?php if($_SESSION['Username']!=''){?>
 		<form style="
 			width:40%;
 			box-sizing:border-box;
@@ -107,6 +74,7 @@ $dataReview = $review->getReview($_GET['book_id']);
 					padding:10px;
 				" type="image" name="WannaRead" src="img/add.png" alt="Хочу прочитати">
 		</form>
+		<?php }?>
 		<div style = "
 			width:100%;
 			float:left;
@@ -114,27 +82,37 @@ $dataReview = $review->getReview($_GET['book_id']);
 		">
 		<?php if($dataReview!=0){ for($i = 0; $i<count($dataReview);$i = $i + 1){?>
 			<div style = "
-			width:70%;
+			width:50%;
 			margin:0 auto;
 			">
+				<div>
+					<p style="
+						background:#006b5d;
+						font-size:1.2em;
+						padding:0.5em 1%;
+						color:white;
+						margin:0;
+						float:left;
+						width:50%;
+						box-sizing:border-box;
+					">Рецензія користувача: <?php echo $review->getUserName($dataReview[$i][2])?></p>
+					<p style="
+						background:#006b5d;
+						font-size:1em;
+						padding:0.6em 1%;
+						color:white;
+						margin:0;
+						float:right;
+						width:50%;
+						box-sizing:border-box;
+						text-align:right;
+					">Дата рецензії: <?php echo $dataReview[$i][4]?></p>
+				</div>
 				<p style="
-					background:#006b5d;
-					font-size:1.3em;
-					padding:0.5em;
-					color:white;
-					margin:0;
-				">Рецензія користувача: <?php echo $review->getUserName($dataReview[$i][2])?></p>
-				<p style="
-					background:#006b5d;
-					font-size:1em;
-					padding:0.5em;
-					color:white;
-					margin:0;
-				">Дата рецензії: <?php echo $dataReview[$i][4]?></p>
-				<p style="
-					padding:0 0.5em;
+					padding-top:0.5em;
 					text-align:justify;
-					text-indent:10%;
+					text-indent:5%;
+					clear:both;
 				">
 				<?php echo $dataReview[$i][1]?>
 				</p>
@@ -145,10 +123,9 @@ $dataReview = $review->getReview($_GET['book_id']);
 		<div style = "
 			width:100%;
 			float:left;
-			padding-top:1em;
 		">
 			<div style = "
-				width:70%;
+				width:50%;
 				margin:0 auto;
 			">
 				<p style="
@@ -160,7 +137,7 @@ $dataReview = $review->getReview($_GET['book_id']);
 				">Рецензія користувача: <?php echo $_SESSION['Username'] . " до книги " . $data[0][1]?></p>
 				<form method="post">
 					<textarea style="
-						width:99.4%;
+						width:100%;
 						margin:0 auto;
 						height:200px;
 						margin:0.2em 0;
@@ -169,6 +146,7 @@ $dataReview = $review->getReview($_GET['book_id']);
 						border-radius:0;
 						resize:none;
 						text-align:justify;
+						box-sizing:border-box;
 					" name="textReview" maxlength="300" placeholder="Напишіть щось обмірковане..."></textarea>
 					<input style="
 						font-size:1.2em;
@@ -187,4 +165,3 @@ $dataReview = $review->getReview($_GET['book_id']);
 <?php
 require_once 'components\footer.php';
 ?>
->>>>>>> d9a2c1a08730a57124c46d483ec04229f5bc631b
