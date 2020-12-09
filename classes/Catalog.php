d478fd3dcaf2f7595409f66c2bbdec5b23e25700
Mxkxk <74319751+Mxkxk@users.noreply.github.com>
2020-12-09 15:36:24 +0200

    Like/Want statuses adding and deleting

--- classes/Catalog.php
+++ classes/Catalog.php
@@ -4,7 +4,7 @@ require_once 'Database.php';
 
 class Catalog extends Database
 {
-
+    
     public function datatable($query){
 
         try{
@@ -13,19 +13,19 @@ class Catalog extends Database
 
 
             for ($i=0;$i<count($data);$i++){
-                    $AutorData =  $this->doQuery('Select FirstName,LastName from Author Where Id='.$data[$i][6]);
-                    echo 'Id '.($i+1)." Назва : <b>"?><html><a href="bookpage.php?book_id=<?php echo($data[$i][0]) ?>"></html><?php echo $data[$i][1].'</b></a> Автор: <b>'.$AutorData[0][0].' '.$AutorData[0][1].'</b> Видавництво: '.$data[$i][2].' Жанр: '.$data[$i][3].' Кількість сторінок: '.$data[$i][4].' В наявності: '.$data[$i][5];
-                    if( $_SESSION['Username']=='admin') {
-                        ?>
+                $AutorData =  $this->doQuery('Select FirstName,LastName from Author Where Id='.$data[$i][6]);
+                echo 'Id '.($i+1)." Назва : <b>"?><html><a href="bookpage.php?book_id=<?php echo($data[$i][0]) ?>"></html><?php echo $data[$i][1].'</b></a> Автор: <b>'.$AutorData[0][0].' '.$AutorData[0][1].'</b> Видавництво: '.$data[$i][2].' Жанр: '.$data[$i][3].' Кількість сторінок: '.$data[$i][4].' В наявності: '.$data[$i][5];
+                if( $_SESSION['Username']=='admin') {
+                    ?>
 
-                        <html>
-                        <a href="edit.php?book_id=<?php echo($data[$i][0]) ?>">Edit</a>/<a
-                                href="delete.php?book_id=<?php echo($data[$i][0]) ?>">Delete</a>
-                        <br><br></html>
-                        <?php
-                    }else{
-                        echo '<br> <br>';
-                    }
+                    <html>
+                    <a href="edit.php?book_id=<?php echo($data[$i][0]) ?>">Edit</a>/<a
+                        href="delete.php?book_id=<?php echo($data[$i][0]) ?>">Delete</a>
+                    <br><br></html>
+                    <?php
+                }else{
+                    echo '<br> <br>';
+                }
             }
         }catch (Exception $a){
             echo 'Table is empty';
@@ -61,7 +61,7 @@ class CreateCatalog extends Catalog {
                 'ai' =>$AuthorId
        ));
    }
-   public function getLastId(){
+   public function getLastId(){	   
 	   $data = $this->doQuery("Select Max(id) From Book");
 	   return $data[0][0];
 	}
@@ -107,42 +107,61 @@ class Review extends Catalog{
     }
 	public function getUserName($usrid){
         $pdo=$this->connect();
-		$revUserName = $this->doQuery("Select UserName From User WHERE Id = " . $usrid);
+		$revUserName = $this->doQuery("Select UserName From User WHERE Id = " . $usrid);		
 		return $revUserName[0][0];
     }
+	public function getStatus($UserName, $BookId){
+		$pdo=$this->connect();
+		$UserId = $this->doQuery("Select Id From User WHERE UserName = '" . $UserName . "'");
+		return $this->doQuery("Select * From StatusCheck WHERE UserId = " . $UserId[0][0] . " AND BookId = " . $BookId);
+	}
+	public function setStatus($Status, $UserName, $BookId){
+		$pdo=$this->connect();
+		$status = $this->getStatus($UserName, $BookId);
+		$used = false;
+		for($i = 0; $i < count($status); $i++){
+			if($status[$i][1] == $Status){
+				$stmt = $pdo->prepare("DELETE FROM StatusCheck WHERE Id = ".$status[$i][0]);//"StatusBook = '".$Status."' AND UserId = ".$status[$i][2]." AND BookId");
+				$stmt->execute();
+				$used = true;				
+				break;
+			}
+		}
+		if(!$used){
+			$UserId = $this->doQuery("Select Id From User WHERE UserName = '" . $UserName . "'");
+			$stmt = $pdo->prepare("INSERT INTO StatusCheck (StatusBook, UserId, BookId) VALUES ('".$Status."',:ui,:bi)");
+			$stmt->execute(array(
+            ':ui' => $UserId[0][0],
+            ':bi' => $BookId
+        ));
+		}
+	}
 }
 
 class EditAndDelete extends Catalog {
     public function findBookName($id){
         $pdo=$this->connect();
         $data=$this->doQuery('Select BookName from Book where Id='.$id);
-            return $data;
+        return $data;
     }
-public function delete ($id){
-    $pdo=$this->connect();
-    $stmt = $pdo->prepare("DELETE FROM Book WHERE Id=".$id);
-    $stmt->execute();
-}
-public function getInfo($id){
-       return $this->doQuery("Select * from Book where Id=".$id);
-}
-    public function getAuthor($id){
-        return $this->doQuery("Select * from Author where Id=".$id);
-    }
-public function edit($BookName,$Publishing,$Genre,$PageNum,$inStock,$id){
-    $pdo=$this->connect();
-    $stmt = $pdo->prepare("UPDATE Book SET BookName='".$BookName."', publishing='".$Publishing."', Genre='".$Genre."', AmountOfPages=".$PageNum.",InStock=".$inStock." WHERE Id=".$id);
-    $stmt->execute();
-}
+	public function delete ($id){
+		$pdo=$this->connect();
+		$stmt = $pdo->prepare("DELETE FROM Book WHERE Id=".$id);
+		$stmt->execute();
+	}
+	public function getInfo($id){
+	    return $this->doQuery("Select * from Book where Id=".$id);
+	}
+	public function getAuthor($id){
+		return $this->doQuery("Select * from Author where Id=".$id);
+	}
+	public function edit($BookName,$Publishing,$Genre,$PageNum,$inStock,$id){
+		$pdo=$this->connect();
+		$stmt = $pdo->prepare("UPDATE Book SET BookName='".$BookName."', publishing='".$Publishing."', Genre='".$Genre."', AmountOfPages=".$PageNum.",InStock=".$inStock." WHERE Id=".$id);
+		$stmt->execute();
+	}
 }
 $editAndDelete = new EditAndDelete();
 $createCatalog = new CreateCatalog();
 $catalog = new Catalog();
 $review = new Review();
-
-
-
-
-
-
-
